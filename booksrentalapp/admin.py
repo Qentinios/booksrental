d250b256ea2a61fa433dbe278cff7fb1ca7722d1
Qentinios <adrian_opole@interia.pl>
2018-09-10 13:54:48 +0200

    Added all tables to django admin panel, updated tables

--- booksrentalapp/admin.py
+++ booksrentalapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Book, Rent, Category, BookCategory
 
-# Register your models here.
+
+class BookCategoryInline(admin.StackedInline):
+    model = BookCategory
+    extra = 1
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'description',  'pub_date', 'author']}),
+    ]
+    inlines = [BookCategoryInline]
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Rent)
+admin.site.register(Category)
--- booksrentalapp/models.py
+++ booksrentalapp/models.py
@@ -2,15 +2,15 @@ from django.conf import settings
 from django.db import models
 
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField
+    description = models.TextField(default=None, null=True)
     pub_date = models.DateTimeField('date published')
     author = models.CharField(max_length=40)
 
 
 class Rent(models.Model):
-    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rent_date = models.DateTimeField('date rented')
     return_date = models.DateTimeField('date returned', default=None, null=True)
@@ -18,6 +18,12 @@ class Rent(models.Model):
 
 class Category(models.Model):
     category = models.CharField(max_length=200)
-    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+
+class BookCategory(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
 
 
